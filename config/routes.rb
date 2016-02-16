faf12a393432efecba5f3889d36f6d59a6ac4685
Theresa Luu <theresa.luu@gmail.com>
2016-02-15 22:56:00 -0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'events/create'
 
-  post '/events' => "events#create"
+  post '/events'        => "events#create"
+  post '/events/clear'  => "events#clear"
+  #get '/events'         => "events#range"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
