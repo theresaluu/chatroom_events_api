786b39c4159d49b27c14cf308451095ef88eaf24
Theresa Luu <theresa.luu@gmail.com>
2016-03-06 03:32:44 -0800

    Added conditional validations based on actions.
    otheruser is required when there is a highfives action.
    message is required when there is a comments action.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ActiveRecord::Base
   validates :user, presence: true
   validates :action, presence: true
   validates :date, presence: true
+  validates :message, presence: true,
+    if: Proc.new { |e| e.action  == 'comments' }
+  validates :otheruser, presence: true,
+    if: Proc.new { |e| e.action == 'highfives' }
 end
--- spec/models/event_test.rb
+++ spec/models/event_test.rb
@@ -4,4 +4,31 @@ RSpec.describe Event, 'Post' do
   it { should validate_presence_of(:user)}
   it { should validate_presence_of(:action)}
   it { should validate_presence_of(:date)}
+
+  describe 'comments validation' do
+    context 'if action is "comments"' do
+      subject { FactoryGirl.build(:event, action: 'comments') }
+
+      it { should validate_presence_of(:message) }
+    end
+
+    context 'if action is not "comments"' do
+      subject { FactoryGirl.build(:event, action: 'enters') }
+
+      it { should_not validate_presence_of(:message) }
+    end
+  end
+
+  describe 'highfives validation' do
+    context 'if action is "highfives"' do
+      subject { FactoryGirl.build(:event, action: 'highfives') }
+
+      it{ should validate_presence_of(:otheruser) }
+    end
+    context 'if action is not "highfives"' do
+      subject { FactoryGirl.build(:event, action: 'leaves') }
+
+      it { should_not validate_presence_of(:otheruser) }
+    end
+  end
 end
