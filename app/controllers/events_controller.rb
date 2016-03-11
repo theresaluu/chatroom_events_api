8e0f4ad79fc571dea670ed40d4ca836e55bb5343
Theresa Luu <theresa.luu@gmail.com>
2016-03-11 00:37:13 -0800

    Created logic to collect dates & count events.
    Currently these methods are not being called upon the request but will
    refactor and implement the call.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -42,4 +42,51 @@ class EventsController < ApplicationController
       Time.parse(params['to'] || params[:to]).utc
     end
   end
+
+  def event_dates(timeframe)
+    event_dates = []
+    Event.all.each do |event|
+      case timeframe
+      when 'minute'
+        event_dates << round_down_seconds(event.date).iso8601
+      when 'hour'
+        event_dates << round_down_minutes(event.date).iso8601
+      when 'day'
+        event_dates << round_down_minutes(event.date).iso8601
+      end
+    end
+    event_dates.uniq.sort_by do |date|
+      date
+    end
+  end
+
+  def round_down_seconds(date)
+    date - date.strftime('%S').to_i.seconds
+  end
+
+  def round_down_minutes(date)
+    round_down_seconds(date) - date.strftime('%M').to_i.minutes
+  end
+
+  def round_down_hours(date)
+    round_down_minutes(date) - date.strftime('%H').to_i.hours
+  end
+
+  def event_actions(event_dates)
+    event_list = []
+    event_dates.each do |date|
+      counts = Hash.new(0)
+      counts[:date] = date
+      event_actions = []
+      Event.all.each do |event|
+        if event.date == date
+          event_actions << event.action
+        end
+      end
+      event_actions.each do |event|
+        counts[event] += 1
+      end
+      event_list << counts
+    end
+  end
 end
